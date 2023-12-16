3d67b9ac0e257204b2602f3c31d2481abf9f2ad1
Aymen LABIADH <aymenldh@gmail.com>
2023-12-16 11:37:07 +0100

    Ajout de movie.model

---
+++ src/app/models/movie.model.ts
@@ -0,0 +1,12 @@
+export class Movie {
+    id?: any;
+    name?: string;
+    overview?: string;
+    backdrop_path?:string
+    original_language?:string
+    popularity?:number
+    vote_average?: number
+    vote_count?: number
+    origin_country?: string []
+    first_air_date?:Date
+}
