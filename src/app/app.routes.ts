f857ec54fcee4f2d020750deb00611d78a73e6c9
Aymen LABIADH <aymenldh@gmail.com>
2023-12-18 15:24:52 +0100

    MàJ du composant movie-details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'movies', pathMatch: 'full' },
+    { path: 'movies', component: MoviesListComponent },
+    { path: 'details', component: MovieDetailsComponent }    
+  ];
